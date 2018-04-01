2ddd77260faa2b205daf3d8724b9bf89dc3e8883
bantyK <bantyp92@gmail.com>
2018-04-01 20:22:46 +0530

    edit task activity layout added

--- app/src/main/java/com/example/vuclip/todo_mvp/addedittask/AddEditTaskActivity.java
+++ app/src/main/java/com/example/vuclip/todo_mvp/addedittask/AddEditTaskActivity.java
@@ -1,10 +1,27 @@
 package com.example.vuclip.todo_mvp.addedittask;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.vuclip.todo_mvp.R;
 
 /**
  * Created by Banty on 31/03/18.
  */
 public class AddEditTaskActivity extends AppCompatActivity {
     public static final int REQUEST_ADD_TASK = 1;
+
+    public static final String SHOULD_LOAD_DATA_FROM_REPO_KEY = "SHOULD_LOAD_DATA_FROM_REPO_KEY";
+
+    private AddEditTaskPresenter mAddEditTaskPresenter;
+
+    private ActionBar mActionBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addtask_activity);
+    }
 }
---
+++ app/src/main/java/com/example/vuclip/todo_mvp/addedittask/AddEditTaskPresenter.java
@@ -0,0 +1,7 @@
+package com.example.vuclip.todo_mvp.addedittask;
+
+/**
+ * Created by Banty on 01/04/18.
+ */
+class AddEditTaskPresenter {
+}
